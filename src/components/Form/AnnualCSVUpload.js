ac3fe3f03c2088245fb4417b543fdbfc143041c3
prinxard <prinxard@gmail.com>
2021-12-17 16:30:26 +0100

    refactored annual filing documents uploads UI

--- src/components/Form/AnnualCSVUpload.js
+++ src/components/Form/AnnualCSVUpload.js
@@ -21,7 +21,7 @@ const AnnualCSVUploadForm = () => {
   const fileHandler = (event) => {
     const file = event.target.files[0];
     if (file) {
-      if (file.type !== "application/vnd.ms-excel") {
+      if (file.type !== "image/jpeg" && file.type !== "application/pdf" && file.type !== "image/png") {
         alert("file type not allowed. only csv(delimited comma) are allowed.");
         setFile(null);
         return;
@@ -32,31 +32,29 @@ const AnnualCSVUploadForm = () => {
       setFile("no file chosen yet");
     }
   };
+
   return (
-
-
     <form>
       <TokenModalsOverlay>
         <TokenModals />
       </TokenModalsOverlay>
-      {/* <SectionTitle title="Schedule Uploads" subtitle="Annual PAYE Returns" /> */}
-      {/* <SectionTitle subtitle="UPLOAD ANNUAL RETURNS DOCUMENTS" /> */}
-      <h6 className="p-2">Correspondence</h6>
+      <h6 className="p-2 font-bold">Correspondence</h6>
       <Widget>
         <div>
           <div>
             <div className="flex justify-between mb-5">
-              <p>Cover letter of submission of annual returns * (pdf, jpg, png)</p>
+              <p>Cover letter of submission of annual returns <span className="font-bold" style={{ color: "red" }}> * </span><small>(pdf, jpg, png)</small> </p>
               <input
                 required
                 type="file"
+                multiple
                 className="hidden"
                 ref={fileInputRef}
                 onChange={fileHandler}
               />
               <div className="flex items-center">
-                <button
-                  className="btn btn-default btn-outlined bg-transparent mr-4"
+                <button style={{ backgroundColor: "#84abeb" }}
+                  className="btn btn-default btn-outlined bg-transparent rounded-md mr-4"
                   onClick={(event) => {
                     event.preventDefault();
                     fileInputRef.current.click();
@@ -70,7 +68,7 @@ const AnnualCSVUploadForm = () => {
             </div>
             <hr className="mb-2" />
             <div className="flex justify-between mb-5">
-              <p>Copy of letter mandating employees to file individual tax returns * (pdf, jpg, png)</p>
+              <p>Copy of letter mandating employees to file individual tax returns <span className="font-bold" style={{ color: "red" }}> * </span> <small>(pdf, jpg, png)</small></p>
               <input
                 required
                 type="file"
@@ -80,7 +78,8 @@ const AnnualCSVUploadForm = () => {
               />
               <div className="flex items-center">
                 <button
-                  className="btn btn-default btn-outlined bg-transparent mr-4"
+                  style={{ backgroundColor: "#84abeb" }}
+                  className="btn btn-default rounded-md btn-outlined bg-transparent mr-4"
                   onClick={(event) => {
                     event.preventDefault();
                     fileInputRef.current.click();
@@ -91,8 +90,9 @@ const AnnualCSVUploadForm = () => {
                 <p>{file ? file.name : "no file chosen yet"}</p>
               </div>
             </div>
+            <hr className="mb-2" />
             <div className="flex justify-between mb-5">
-              <p>Letter of expatriate order [where applicable]  (pdf, jpg, png)</p>
+              <p>Letter of expatriate order [where applicable]  <small>(pdf, jpg, png)</small> </p>
               <input
                 required
                 type="file"
@@ -102,7 +102,8 @@ const AnnualCSVUploadForm = () => {
               />
               <div className="flex items-center">
                 <button
-                  className="btn btn-default btn-outlined bg-transparent mr-4"
+                  style={{ backgroundColor: "#84abeb" }}
+                  className="btn btn-default rounded-md btn-outlined bg-transparent mr-4"
                   onClick={(event) => {
                     event.preventDefault();
                     fileInputRef.current.click();
@@ -118,11 +119,11 @@ const AnnualCSVUploadForm = () => {
         </div>
       </Widget>
       <div className="mt-12">
-        <h6 className="p-2">Remittance</h6>
+        <h6 className="p-2 font-bold">Remittance</h6>
       </div>
       <Widget className="mt-8">
         <div className="flex justify-between mb-5">
-          <p>Schedule of withholding tax deductions * (excel)</p>
+          <p>Schedule of withholding tax deductions <span className="font-bold" style={{ color: "red" }}> * </span> <small> (excel, pdf)</small><br /><span className="flex justify-end" style={{color: "blue"}}><Link href="/csv/wht.csv"> download </Link></span></p>
           <input
             required
             type="file"
@@ -132,7 +133,8 @@ const AnnualCSVUploadForm = () => {
           />
           <div className="flex items-center">
             <button
-              className="btn btn-default btn-outlined bg-transparent mr-4"
+              style={{ backgroundColor: "#84abeb" }}
+              className="btn btn-default rounded-md btn-outlined bg-transparent mr-4"
               onClick={(event) => {
                 event.preventDefault();
                 fileInputRef.current.click();
@@ -145,7 +147,7 @@ const AnnualCSVUploadForm = () => {
         </div>
         <hr className="mb-2" />
         <div className="flex justify-between mb-5">
-          <p>Withholding tax receipts (corporate & Individual) * (pdf, jpg, png)</p>
+          <p>Withholding tax receipts (corporate & Individual) <span className="font-bold" style={{ color: "red" }}> * </span> <small>(pdf, jpg, png)</small></p>
           <input
             required
             type="file"
@@ -155,7 +157,8 @@ const AnnualCSVUploadForm = () => {
           />
           <div className="flex items-center">
             <button
-              className="btn btn-default btn-outlined bg-transparent mr-4"
+              style={{ backgroundColor: "#84abeb" }}
+              className="btn btn-default rounded-md btn-outlined bg-transparent mr-4"
               onClick={(event) => {
                 event.preventDefault();
                 fileInputRef.current.click();
@@ -168,7 +171,7 @@ const AnnualCSVUploadForm = () => {
         </div>
         <hr className="mb-2" />
         <div className="flex justify-between mb-5">
-          <p>Monthly Immigration returns [where applicable] (pdf, jpg, png)</p>
+          <p>Monthly Immigration returns [where applicable] <small>(pdf, jpg, png)</small></p>
           <input
             required
             type="file"
@@ -178,7 +181,8 @@ const AnnualCSVUploadForm = () => {
           />
           <div className="flex items-center">
             <button
-              className="btn btn-default btn-outlined bg-transparent mr-4"
+              style={{ backgroundColor: "#84abeb" }}
+              className="btn btn-default rounded-md btn-outlined bg-transparent mr-4"
               onClick={(event) => {
                 event.preventDefault();
                 fileInputRef.current.click();
@@ -191,10 +195,10 @@ const AnnualCSVUploadForm = () => {
         </div>
         <hr className="mb-2" />
       </Widget>
-      <div className="mt-12"><h6 className="p-2">Contributions and levies</h6></div>
+      <div className="mt-12"><h6 className="p-2 font-bold">Contributions and levies</h6></div>
       <Widget>
         <div className="flex justify-between mb-5">
-          <p>Development levy receipts (corporate & Individual) * (pdf, jpg, png) </p>
+          <p>Development levy receipts (corporate & Individual) <span className="font-bold" style={{ color: "red" }}> * </span> <small>(pdf, jpg, png)</small>  </p>
           <input
             required
             type="file"
@@ -204,7 +208,8 @@ const AnnualCSVUploadForm = () => {
           />
           <div className="flex items-center">
             <button
-              className="btn btn-default btn-outlined bg-transparent mr-4"
+              style={{ backgroundColor: "#84abeb" }}
+              className="btn btn-default rounded-md btn-outlined bg-transparent mr-4"
               onClick={(event) => {
                 event.preventDefault();
                 fileInputRef.current.click();
@@ -217,7 +222,7 @@ const AnnualCSVUploadForm = () => {
         </div>
         <hr className="mb-2" />
         <div className="flex justify-between mb-5">
-          <p>Business premises receipts (corporate & Individual) * (pdf, jpg, png) </p>
+          <p>Business premises receipts (corporate & Individual) <span className="font-bold" style={{ color: "red" }}> * </span> <small>(pdf, jpg, png</small> ) </p>
           <input
             required
             type="file"
@@ -227,7 +232,8 @@ const AnnualCSVUploadForm = () => {
           />
           <div className="flex items-center">
             <button
-              className="btn btn-default btn-outlined bg-transparent mr-4"
+              style={{ backgroundColor: "#84abeb" }}
+              className="btn btn-default rounded-md btn-outlined bg-transparent mr-4"
               onClick={(event) => {
                 event.preventDefault();
                 fileInputRef.current.click();
@@ -240,7 +246,7 @@ const AnnualCSVUploadForm = () => {
         </div>
         <hr className="mb-2" />
         <div className="flex justify-between mb-5">
-          <p>Ground rent receipts (corporate & Individual) * (pdf, jpg, png)</p>
+          <p>Ground rent receipts (corporate & Individual) <span className="font-bold" style={{ color: "red" }}> * </span> <small>(pdf, jpg, png)</small> </p>
           <input
             required
             type="file"
@@ -250,7 +256,8 @@ const AnnualCSVUploadForm = () => {
           />
           <div className="flex items-center">
             <button
-              className="btn btn-default btn-outlined bg-transparent mr-4"
+              style={{ backgroundColor: "#84abeb" }}
+              className="btn btn-default rounded-md btn-outlined bg-transparent mr-4"
               onClick={(event) => {
                 event.preventDefault();
                 fileInputRef.current.click();
@@ -263,7 +270,7 @@ const AnnualCSVUploadForm = () => {
         </div>
         <hr className="mb-2" />
         <div className="flex justify-between mb-5">
-          <p>Social service contributions levy (SSCL) (corporate & Individual) * (pdf, jpg, png) </p>
+          <p>Social service contributions levy (SSCL) (corporate & Individual) <span className="font-bold" style={{ color: "red" }}> * </span> <small>(pdf, jpg, png</small> ) </p>
           <input
             required
             type="file"
@@ -273,7 +280,8 @@ const AnnualCSVUploadForm = () => {
           />
           <div className="flex items-center">
             <button
-              className="btn btn-default btn-outlined bg-transparent mr-4"
+              style={{ backgroundColor: "#84abeb" }}
+              className="btn btn-default rounded-md btn-outlined bg-transparent mr-4"
               onClick={(event) => {
                 event.preventDefault();
                 fileInputRef.current.click();
@@ -285,10 +293,10 @@ const AnnualCSVUploadForm = () => {
           </div>
         </div>
       </Widget>
-      <div className="mt-12"><h6 className="p-2">Deductions</h6></div>
+      <div className="mt-12"><h6 className="p-2 font-bold">Deductions</h6></div>
       <Widget>
         <div className="flex justify-between mb-5">
-          <p>Evidence of remittance of pension * (pdf, jpg, png) </p>
+          <p>Evidence of remittance of pension <span className="font-bold" style={{ color: "red" }}> * </span> <small>(pdf, jpg, png)</small>  </p>
           <input
             required
             type="file"
@@ -298,7 +306,8 @@ const AnnualCSVUploadForm = () => {
           />
           <div className="flex items-center">
             <button
-              className="btn btn-default btn-outlined bg-transparent mr-4"
+              style={{ backgroundColor: "#84abeb" }}
+              className="btn btn-default rounded-md btn-outlined bg-transparent mr-4"
               onClick={(event) => {
                 event.preventDefault();
                 fileInputRef.current.click();
@@ -311,7 +320,7 @@ const AnnualCSVUploadForm = () => {
         </div>
         <hr className="mb-2" />
         <div className="flex justify-between mb-5">
-          <p>Evidence of remittance of NHF * (pdf, jpg, png) </p>
+          <p>Evidence of remittance of NHF <span className="font-bold" style={{ color: "red" }}> * </span> <small>(pdf, jpg, png)</small>  </p>
           <input
             required
             type="file"
@@ -321,7 +330,8 @@ const AnnualCSVUploadForm = () => {
           />
           <div className="flex items-center">
             <button
-              className="btn btn-default btn-outlined bg-transparent mr-4"
+              style={{ backgroundColor: "#84abeb" }}
+              className="btn btn-default rounded-md btn-outlined bg-transparent mr-4"
               onClick={(event) => {
                 event.preventDefault();
                 fileInputRef.current.click();
@@ -333,7 +343,7 @@ const AnnualCSVUploadForm = () => {
           </div>
         </div>
         <div className="flex justify-between mb-5">
-          <p>Evidence of remittance of NHIS *</p>
+          <p>Evidence of remittance of NHIS <span className="font-bold" style={{ color: "red" }}> * </span></p>
           <input
             required
             type="file"
@@ -343,7 +353,8 @@ const AnnualCSVUploadForm = () => {
           />
           <div className="flex items-center">
             <button
-              className="btn btn-default btn-outlined bg-transparent mr-4"
+              style={{ backgroundColor: "#84abeb" }}
+              className="btn btn-default rounded-md btn-outlined bg-transparent mr-4"
               onClick={(event) => {
                 event.preventDefault();
                 fileInputRef.current.click();
@@ -355,7 +366,7 @@ const AnnualCSVUploadForm = () => {
           </div>
         </div>
         <div className="flex justify-between mb-5">
-          <p>Evidence of remittance of LAP * (pdf, jpg, png)</p>
+          <p>Evidence of remittance of LAP <span className="font-bold" style={{ color: "red" }}> * </span> <small>(pdf, jpg, png)</small> </p>
           <input
             required
             type="file"
@@ -365,7 +376,8 @@ const AnnualCSVUploadForm = () => {
           />
           <div className="flex items-center">
             <button
-              className="btn btn-default btn-outlined bg-transparent mr-4"
+              style={{ backgroundColor: "#84abeb" }}
+              className="btn btn-default rounded-md btn-outlined bg-transparent mr-4"
               onClick={(event) => {
                 event.preventDefault();
                 fileInputRef.current.click();
--- src/config/url.js
+++ src/config/url.js
@@ -1,10 +1,10 @@
 const url = {
   //localhost
-  BASE_URL: "http://localhost:3003/api/v1/",
+  // BASE_URL: "http://localhost:3003/api/v1/",
   // PAY_URL: 'http://localhost/etaxpay/',
 
   // live db
-  // BASE_URL: "https://portalapi.bespoque.dev/api/v1/",
+  BASE_URL: "https://portalapi.bespoque.dev/api/v1/",
 
   // PAY_URL: "https://etaxpay.irs.kg.gov.ng/",
 };
