08ca18730977638b89a43d91b1ebb66eee9975a4
prinxard <prinxard@gmail.com>
2022-01-13 16:24:35 +0100

    changed monthly upload year selection

--- src/components/forms/selects.js
+++ src/components/forms/selects.js
@@ -4,10 +4,10 @@ export const Select = React.forwardRef((props, ref) => (
   <div className={`form-element ${props.inline ? 'form-element-inline' : ''}`}>
     <div className="form-label">{props.label}</div>
     <select className="form-select" {...props} ref={ref}>
-      <option value="2018">2017</option>
-      <option value="2018">2018</option>
       <option value="2019">2019</option>
       <option value="2020">2020</option>
+      <option value="2021">2021</option>
+      <option value="2022">2022</option>
       {/* <option value="2021">2021</option> */}
     </select>
   </div>
