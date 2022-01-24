9e7b2cd4b87da01350e921f140d06a76fe7ead5b
prinxard <prinxard@gmail.com>
2022-01-24 21:06:00 +0100

    changed annual table

--- src/components/tables/viewAnnual.js
+++ src/components/tables/viewAnnual.js
@@ -2,66 +2,32 @@ import Widget from "../widget";
 import { formatNumber } from "../../functions/numbers";
 import * as Icons from '../../components/Icons/index';
 import Widget1 from "../dashboard/widget-1";
+import dateformat from "dateformat";
+import Link from 'next/link';
 
 const fields = [
-  // {
-  //   name: "Tax ID",
-  //   key: "id",
-  // },
-  {
-    name: "Staff names",
-    key: "staff_names",
-  },
-  {
-    name: "Number of Months",
-    key: "no_months",
-  },
-  {
-    name: "Baic salary",
-    key: "basic_salary",
-  },
-  {
-    name: "Pension",
-    key: "pension",
-  },
-  {
-    name: "NHIS",
-    key: "nhis",
-  },
-  {
-    name: "LAP",
-    key: "lap",
-  },
-  {
-    name: "NHF",
-    key: "nhf",
-  },
-  {
-    name: "Consolidated Relief Allowance",
-    key: "con_rel_cal",
-  },
-  {
-    name: "Net tax deducted",
-    key: "net_tax_ded",
-  },
-  {
-    name: "Expected tax",
-    key: "tax_pay_cal",
-  },
-  {
-    name: "Variance",
-    key: "variance_cal",
-  },
   {
     name: "Year",
     key: "year",
+  },
+  {
+    name: "Basic Salary",
+    key: "basicSalary",
+  },
+  {
+    name: "Net Tax deducted",
+    key: "netTaxDeduct",
+  },
+  {
+    name: "Total Tax",
+    key: "tax_pay_cal",
   },
 
 ];
 
 export const ViewAnnualTable = ({ remittance, totalemployees, totaltax, grosssum }) => {
   let items = remittance;
-  // console.log(items)
+  console.log(remittance)
   remittance.map((remittance) => {
     remittance["amount"] = formatNumber(remittance["amount"]);
     if (remittance["status"] === 1) {
@@ -74,50 +40,6 @@ export const ViewAnnualTable = ({ remittance, totalemployees, totaltax, grosssum
 
   return (
     <>
-{/* 
-      <Widget>
-        <div className="flex flex-col lg:flex-row w-full lg:space-x-2 space-y-2 lg:space-y-0 mb-2 lg:mb-4">
-
-          <div className="w-full lg:w-1/4">
-            <Widget1
-              color="green"
-              title="2020"
-              // description={formatNumber(data[0].totalRemittance)}
-              right={<Icons.TotalRemittance />}
-            />
-          </div>
-
-          <div className="w-full lg:w-1/4">
-            <Widget1
-              color="red"
-              title="Pending Remittance"
-              // description={formatNumber(data[0].pendingRemittance)}
-              right={<Icons.PendingRemittance />}
-            />
-          </div>
-
-          <div className="w-full lg:w-1/4">
-            <Widget1
-              color="blue"
-              title="Revenue Items"
-              // description={formatNumber(data[0].revenueItems)}
-              right={<Icons.RevenueItems />}
-            />
-          </div>
-
-          <div className="w-full lg:w-1/4">
-            <Widget1
-              color="yellow"
-              title="Tax receipts"
-              // description={formatNumber(data[0].taxReceipts)}
-              right={<Icons.TaxReceipt />}
-            />
-          </div>
-        </div>
-
-      </Widget> */}
-
-
       <Widget>
         <table className="table divide-y">
           <thead>
@@ -134,12 +56,12 @@ export const ViewAnnualTable = ({ remittance, totalemployees, totaltax, grosssum
               <tr key={i} className="">
                 {fields.map((field, j) => (
                   <td key={j} className="">
-                    {remittance[field.key]}
-                    {/* <Link href={`/dashboard/${remittance["ref"]}`}>
+                    {/* {remittance[field.key]} */}
+                    <Link href={`/view/annual/${remittance.year}`}>
                       <a className="hover:text-blue-500">
                         {remittance[field.key]}
                       </a>
-                    </Link> */}
+                    </Link>
                   </td>
                 ))}
               </tr>
@@ -169,6 +91,100 @@ export const ViewAnnualTable = ({ remittance, totalemployees, totaltax, grosssum
           <p className="px-6">Variance</p>
         </div>
         {/* <div>{total}</div> */}
+      </Widget>
+    </>
+  );
+};
+
+const singleFields = [
+  // { name: 'Status', key: 'status' },
+
+  {
+    name: 'Staff Name',
+    key: 'staff_names',
+  },
+  {
+    name: 'Number of months',
+    key: 'no_months',
+  },
+  {
+    name: 'Basic Salary',
+    key: 'basic_salary',
+  },
+  {
+    name: 'Pension',
+    key: 'pension',
+  },
+  {
+    name: 'NHIS',
+    key: 'nhis',
+  },
+
+  {
+    name: 'LAP',
+    key: 'lap',
+  },
+
+  {
+    name: 'CONSOLIDATED RELIEF ALLOWANCE',
+    key: 'con_rel_cal',
+  },
+  {
+    name: 'Net Tax Deducted',
+    key: 'net_tax_ded',
+  },
+  {
+    name: 'Expected Tax',
+    key: 'tax_pay_cal',
+  },
+
+  {
+    name: 'Year',
+    key: 'year',
+  },
+];
+
+export const ViewAnnualTableSingle = ({ remittance, total }) => {
+  const items = remittance;
+
+  return (
+    <>
+      <Widget>
+        <div className="overflow-x-auto">
+          <table className="table divide-y">
+            <thead className="">
+              <tr className="font-semibold text-blue-400">
+                {singleFields.map((field, i) => (
+                  <th key={i} className="">
+                    {field.name}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody className="divide-y">
+              {items.map((remittance, i) => (
+                <tr key={i} className="">
+                  {singleFields.map((field, j) => (
+                    <td key={j} className="">
+                      {remittance[field.key]}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+              {items.length > 0 && (
+                <tr className="font-semibold">
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  {/* <td>{formatNumber(total.totalSalary)}</td>
+                  <td>{formatNumber(total.totalChargeable)}</td>
+                  <td>{formatNumber(total.totalRelief)}</td>
+                  <td>{formatNumber(total.totalTax)}</td> */}
+                </tr>
+              )}
+            </tbody>
+          </table>
+        </div>
       </Widget>
     </>
   );
--- src/config/url.js
+++ src/config/url.js
@@ -4,11 +4,11 @@ const url = {
   // PAY_URL: 'http://localhost/etaxpay/',
 
   // live db
-  BASE_URL: "https://portallive.bespoque.dev/api/v1/",
-  PAY_URL: "https://etaxpay.irs.kg.gov.ng/",
+  // BASE_URL: "https://portallive.bespoque.dev/api/v1/",
+  // PAY_URL: "https://etaxpay.irs.kg.gov.ng/",
 
   // Test Db
-  // BASE_URL: "https://portalapi.bespoque.dev/api/v1/",
+  BASE_URL: "https://portalapi.bespoque.dev/api/v1/",
 };
 
 export default url;
