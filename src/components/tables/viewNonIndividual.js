1b725e7e7220f3aa03b0e2e703249d09c6146f12
prinxard <prinxard@gmail.com>
2022-02-10 13:28:00 +0100

    added single non-individual payer view

--- src/components/tables/viewNonIndividual.js
+++ src/components/tables/viewNonIndividual.js
@@ -150,49 +150,89 @@ export const ViewSingleNonIndividualTable = ({ indvdata }) => {
 
             <tbody className="divide-y ">
               <tr className="">
-               <td className="">Test1</td>
-               <td>Test</td>
+                <td>KGTIN</td>
+                <td>{items.KGTIN}</td>
               </tr>
               <tr className="">
-               <td>Test1</td>
-               <td>Test</td>
+                <td>Company TIN</td>
+                <td>{items.companytin}</td>
               </tr>
               <tr className="">
-               <td>Test1</td>
-               <td>Test</td>
+                <td>Company Name</td>
+                <td>{items.coy_name}</td>
               </tr>
               <tr className="">
-               <td>Test1</td>
-               <td>Test</td>
+                <td>City</td>
+                <td>{items.city}</td>
               </tr>
               <tr className="">
-               <td>Test1</td>
-               <td></td>
+                <td>Date of incorporation</td>
+                <td>{items.date_of_incorporation}</td>
               </tr>
               <tr className="">
-               <td>Test1</td>
-               <td></td>
+                <td>Line of Business</td>
+                <td>{items.line_of_business}</td>
               </tr>
               <tr className="">
-               <td>Test1</td>
-               <td>Test</td>
+                <td>Type of Organization</td>
+                <td>{items.type_of_organisation}</td>
+              </tr>
+              <tr className="">
+                <td>Enterprise Reg No</td>
+                {items.enterprise_reg_no == null || items.enterprise_reg_no == "" ?
+                  <td>-</td>
+                  : <td>{items.enterprise_reg_no}</td>}
+              </tr>
+              <tr className="">
+                <td>RC No</td>
+                {items.rcno == null || items.rcno == "" ?
+                  <td>-</td>
+                  : <td>{items.rcno}</td>}
+              </tr>
+              <tr className="">
+                <td>State</td>
+                <td>{items.state}</td>
+              </tr>
+              <tr className="">
+                <td>Tax Authority</td>
+                <td>{items.tax_authority}</td>
+              </tr>
+              <tr className="">
+                <td>Tax Office</td>
+                <td>{items.tax_office}</td>
+              </tr>
+              <tr className="">
+                <td>LGA</td>
+                <td>{items.lga}</td>
+              </tr>
+              <tr className="">
+                <td>Phone</td>
+                {items.phone_no == null || items.phone_no == "" ?
+                  <td>-</td>
+                  : <td>{items.phone_no}</td>}
+              </tr>
+              <tr className="">
+                <td>Email</td>
+                {items.e_mail == null || items.e_mail == "" ?
+                  <td>-</td>
+                  : <td>{items.e_mail}</td>}
               </tr>
             </tbody>
           </table>
-       
+
         </div>
         <div className="flex justify-end">
-            <div className="m-3 bg-green-400 text-white rounded-full">
-              <CustomButton type="Submit">
-                Print certificate
-              </CustomButton>
-            </div>
-            <div className="m-3 bg-green-400 text-white rounded-full">
-              <CustomButton type="Submit">
-                Edit
-              </CustomButton>
-            </div>
+          <div className="m-3 bg-green-400 text-white rounded-full">
+            <CustomButton type="Submit">
+              Print certificate
+            </CustomButton>
           </div>
+          <div className="m-3 bg-green-400 text-white rounded-full">
+            <CustomButton type="Submit">
+              Edit
+            </CustomButton>
+          </div>
+        </div>
       </Widget>
     </>
   );
