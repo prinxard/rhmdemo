703a124cf830e2916af844360b0c4ec3430537af
prince unachukwu <prinxard@gmail.com>
2022-05-20 17:42:14 +0100

    added report nav

--- src/components/left-sidebar-1/index.js
+++ src/components/left-sidebar-1/index.js
@@ -7,7 +7,7 @@ import jwt from 'jsonwebtoken';
 
 const LeftSidebar = () => {
   const { navigation, authentication, individualNavigation,
-    navigationApprover, navigationCreator, navigationCreatorApprover, navigationAdmin } = useSelector(
+    navigationApprover, navigationCreator, navigationCreatorApprover, navigationAdmin, navigationReport } = useSelector(
       (state) => ({
         navigationAdmin: state.navigationAdmin,
         navigation: state.navigation,
@@ -16,6 +16,7 @@ const LeftSidebar = () => {
         navigationApprover: state.navigationApprover,
         navigationCreator: state.navigationCreator,
         navigationCreatorApprover: state.navigationCreatorApprover,
+        navigationReport: state.navigationReport,
       }),
       shallowEqual
     );
@@ -23,6 +24,7 @@ const LeftSidebar = () => {
   let approverRange = [1, 2, 3, 12]
   let creatorRange = [1, 4, 13, 15]
   let adminRange = [1]
+  let reportRange = [39, 1]
 
   let StaffType;
   if (authentication) {
@@ -35,6 +37,53 @@ const LeftSidebar = () => {
       <div className="left-sidebar left-sidebar-1">
         <Logo />
         {navigationAdmin.map((menu, i) => (
+          <React.Fragment key={i}>
+            <Title>{menu.title}</Title>
+            <ul>
+              {menu.items.map((l0, a) => (
+                <li key={a} className="l0">
+                  <Item {...l0} />
+                  <ul>
+                    {l0.items.map((l1, b) => (
+                      <li key={b} className="l1">
+                        <Item {...l1} />
+                        <ul className="">
+                          {l1.items.map((l2, c) => (
+                            <li key={c} className="">
+                              <Item {...l2} />
+                              {/* <ul>
+                              {l2.items.map((l3, d) => (
+                                <li key={d} className="l3">
+                                  <Item {...l3} />
+                                  <ul>
+                                    {l3.items.map((l4, e) => (
+                                      <li key={e} className="l4">
+                                        <Item {...l4} />
+                                      </li>
+                                    ))}
+                                  </ul>
+                                </li>
+                              ))}
+                            </ul> */}
+                            </li>
+                          ))}
+                        </ul>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          </React.Fragment>
+        ))}
+      </div>
+    );
+  }
+  if (StaffType.some(r => reportRange.includes(r)) ) {
+    return (
+      <div className="left-sidebar left-sidebar-1">
+        <Logo />
+        {navigationReport.map((menu, i) => (
           <React.Fragment key={i}>
             <Title>{menu.title}</Title>
             <ul>
