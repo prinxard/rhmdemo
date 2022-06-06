eda9e9317681429cbb73fad427afc26e2ab8de56
prince unachukwu <prinxard@gmail.com>
2022-06-06 12:01:48 +0100

    made collections mobile responsive

--- src/components/tables/reports.js
+++ src/components/tables/reports.js
@@ -106,6 +106,7 @@ export const StartReportView = () => {
   let startFigure = watch("amountStart", "0").replace(/,/g, '')
   let endFigure = watch("amountEnd", "0").replace(/,/g, '');
 
+
   useEffect(() => {
 
     setAuthToken();
@@ -156,8 +157,8 @@ export const StartReportView = () => {
       <div className="">
         <form onSubmit={handleSubmit(AdvancedSearch)} className="mb-3">
 
-          <div className="flex">
-            <div className="border mr-2 block p-6 rounded-lg bg-white w-full">
+          <div className="flex flex-col lg:flex-row w-full lg:space-x-2 space-y-2 lg:space-y-0 mb-2 lg:mb-4">
+            <div className="w-full lg:w-1/3 max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-4">
               <div className="mb-2">
                 <p className="font-bold text-center my-2">Search by IDs</p>
                 <label className="" htmlFor="kgtin"> Taxpayer ID</label>
@@ -226,30 +227,26 @@ export const StartReportView = () => {
               </div>
             </div>
 
-            <div className="border block p-6 rounded-lg bg-white w-full">
-              <p className="font-bold text-center mb-5">Assessment Period (start - end)</p>
-              <DateRangePicker
-                onChange={item => setState([item.selection])}
-                showSelectionPreview={true}
-                moveRangeOnFirstSelection={false}
-                months={1}
-                ranges={state}
-                direction="horizontal"
-              />
-              <div className="flex my-4">
-                <div className="grid grid-cols-2 gap-4 justify-self-center">
-                  <div className="form-group">
-                    <button className="btn w-32 bg-green-600 btn-default text-white btn-outlined bg-transparent rounded-md"
-                      type="submit"
-                    >
-                      Search
-                    </button>
-                  </div>
+            <div className="w-full lg:w-2/3">
+              <div className="overflow-x-auto max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-4">
+                <p className="font-bold text-center mb-5">Assessment Period (start - end)</p>
+                <DateRangePicker
+                  onChange={item => setState([item.selection])}
+                  showSelectionPreview={true}
+                  moveRangeOnFirstSelection={false}
+                  months={1}
+                  ranges={state}
+                  direction="vertical"
+                />
+                <div className="my-4">
+                  <button className="btn w-32 bg-green-600 btn-default text-white btn-outlined bg-transparent rounded-md"
+                    type="submit"
+                  >
+                    Search
+                  </button>
                 </div>
               </div>
-
             </div>
-
           </div>
 
 
