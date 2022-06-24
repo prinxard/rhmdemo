2c06944c44cf308582c9141ffa70ae9552514dcb
prince unachukwu <prinxard@gmail.com>
2022-06-24 12:33:35 +0100

    added signatureb to TCC print

--- src/components/tables/viewTccTablePrint.js
+++ src/components/tables/viewTccTablePrint.js
@@ -27,7 +27,7 @@ import Loader from "react-loader-spinner";
 import url from '../../config/url';
 import axios from "axios";
 import ReactToPrint from "react-to-print";
-import { CoatOfArms, KgirsLogo, KogiGov, TccbgImage } from "../Images/Images";
+import { CoatOfArms, KgirsLogo, KogiGov, Signature, TccbgImage } from "../Images/Images";
 import QRCode from "react-qr-code";
 
 
@@ -169,7 +169,7 @@ export const ViewSingleTccPrintTable = ({
   let year2
   let year3
 
-  let basdocurl = 'https://annualuploads.bespoque.dev/rhm/uploads/da/tcc/'
+  let basdocurl = 'https://annualuploads.bespoque.dev/rhm-live/uploads/da/tcc/'
   let picUpload
   let signature
   let fileRef
@@ -505,6 +505,7 @@ export const ViewSingleTccPrintTable = ({
                 </div>
                 <div className="flex justify-between mt-4">
                   <div className="flex flex-col">
+                    <Signature />
                     <hr />
                     <p className="font-bold text-center">Sule Salihu Enehe</p>
                     <p className="font-bold text-center">Ag. Executive Chairman</p>
--- src/pages/payer-profile/[ref].js
+++ src/pages/payer-profile/[ref].js
@@ -173,7 +173,31 @@ export default function SinglePayerProfile() {
             <div className="flex items-center">
               <div className="max-w-7xl w-full mx-auto py-6 sm:px-6 lg:px-8">
                 <div className="flex  flex-col lg:flex-row w-full lg:space-x-2 space-y-2 lg:space-y-0 mb-2 lg:mb-4">
-                  <div className="w-full lg:w-2/4">
+                  <div className="w-full lg:w-1/4">
+                    <div className="widget w-full p-4 rounded-lg bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-800">
+                      <div className="flex flex-row items-center justify-between">
+                        <div className="flex flex-col">
+                          <div className="text-xs uppercase font-light text-gray-500">
+                             Amount Oustanding
+                          </div>
+                          <div className="text-xl font-bold">
+                            {formatNumber(Number(ind.assessmentAmount) - Number((ind.amountPaid)))}
+                          </div>
+                        </div>
+                        <svg className="stroke-current text-gray-500" fill="none" height="24" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewbox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                          <path d="M17 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2">
+                          </path>
+                          <circle cx="9" cy="7" r="4">
+                          </circle>
+                          <path d="M23 21v-2a4 4 0 0 0-3-3.87">
+                          </path>
+                          <path d="M16 3.13a4 4 0 0 1 0 7.75">
+                          </path>
+                        </svg>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="w-full lg:w-1/4">
                     <div className="widget w-full p-4 rounded-lg bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-800">
                       <div className="flex flex-row items-center justify-between">
                         <div className="flex flex-col">
@@ -197,7 +221,7 @@ export default function SinglePayerProfile() {
                       </div>
                     </div>
                   </div>
-                  <div className="w-full lg:w-1/4">
+                  {/* <div className="w-full lg:w-1/4">
                     <div className="widget w-full p-4 rounded-lg bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-800">
                       <div className="flex flex-row items-center justify-between">
                         <div className="flex flex-col">
@@ -214,8 +238,8 @@ export default function SinglePayerProfile() {
                         </svg>
                       </div>
                     </div>
-                    </div>
-                  <div className="w-full lg:w-2/4">
+                  </div> */}
+                  <div className="w-full lg:w-1/4">
                     <div className="widget w-full p-4 rounded-lg bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-800">
                       <div className="flex flex-row items-center justify-between">
                         <div className="flex flex-col">
