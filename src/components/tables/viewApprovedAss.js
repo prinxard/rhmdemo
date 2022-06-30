c0ea7ff804e81ac34b594a96922f2adf1528277f
prince unachukwu <prinxard@gmail.com>
2022-06-30 11:49:23 +0100

    fixed cal on net tax payable

--- src/components/tables/viewApprovedAss.js
+++ src/components/tables/viewApprovedAss.js
@@ -744,7 +744,7 @@ export const ViewSingleApprovedTable = React.forwardRef((props, ref) => {
                         </tr>
                         <tr width="300">
                           <td width="300" className="font-bold tb">Net Tax Payable</td>
-                          <td width="300" className="font-bold tb">{formatNumber(((Number(assobj.tax) + Number(addAssAmount)) + Number(assobj.dev_levy)))}</td>
+                          <td width="300" className="font-bold tb">{formatNumber(((Number(taxcal.tax) + Number(addAssAmount)) + Number(assobj.dev_levy)))}</td>
                         </tr>
                         <tr width="300">
                           <td width="300" className="font-bold tb">Payment due date</td>
