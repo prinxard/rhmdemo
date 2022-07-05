4a8f8e9f4d88a2980a46b0c7dd995f39b0c5120f
prince unachukwu <prinxard@gmail.com>
2022-07-05 10:12:44 +0100

    hide tcc menu from other users

--- src/redux/navigation/navigationApprover.js
+++ src/redux/navigation/navigationApprover.js
@@ -118,27 +118,27 @@ const initialState = [
               },
             ],
           },
-          {
-            url: '/',
-            title: 'Tax Clearance (TCC)',
-            items: [
-              // {
-              //   url: '/tcc',
-              //   title: 'Create',
-              //   items: [],
-              // },
-              {
-                url: '/view/listtcc',
-                title: 'View',
-                items: [],
-              },
-              {
-                url: '/view/listprinttcc',
-                title: 'Print',
-                items: [],
-              },
-            ],
-          },
+          // {
+          //   url: '/',
+          //   title: 'Tax Clearance (TCC)',
+          //   items: [
+          //     // {
+          //     //   url: '/tcc',
+          //     //   title: 'Create',
+          //     //   items: [],
+          //     // },
+          //     {
+          //       url: '/view/listtcc',
+          //       title: 'View',
+          //       items: [],
+          //     },
+          //     {
+          //       url: '/view/listprinttcc',
+          //       title: 'Print',
+          //       items: [],
+          //     },
+          //   ],
+          // },
         ],
       },
 
