c0494da1e0818d3d883c394215bcc5c92f2af8b6
prince unachukwu <prinxard@gmail.com>
2022-07-28 11:19:01 +0100

    changed dev levy boj amount

--- src/components/bojForm/viewBOJForms.js
+++ src/components/bojForm/viewBOJForms.js
@@ -319,11 +319,9 @@ export const StartBOJ = () => {
 
   console.log("T", tax_paid);
 
-  if (tax_paid <= 50000) {
-    dev_levy = "500"
-  } else {
+  
     dev_levy = "1000"
-  }
+  
   // console.log("dev_levy", dev_levy);
 
 
