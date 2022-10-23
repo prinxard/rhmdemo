26a368291f4183dd6aa374b7efda1eb572446385
prince unachukwu <prinxard@gmail.com>
2022-10-23 23:43:41 +0100

    added leave allowance

--- src/components/tables/viewPayslipTable.js
+++ src/components/tables/viewPayslipTable.js
@@ -389,7 +389,7 @@ export const ViewSinglePayslip = ({ paySlipData }) => {
 
                         <div class="col-span-6 sm:col-span-3">
                           <label for="last-name" class="block text-sm font-medium text-gray-700">Leave Allowance</label>
-                          <input type="text" readOnly defaultValue={formatNumber(leave_allw)} class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
+                          <input type="text" readOnly defaultValue={formatNumber(data.leave_allw)} class="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm" />
                         </div>
                         <div class="col-span-6 sm:col-span-3">
                           <label for="last-name" class="block text-sm font-medium text-gray-700">Transport Allowance</label>
