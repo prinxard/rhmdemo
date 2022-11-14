fabfb4b8808619284bb18414d97a913fbd80f0ed
prince unachukwu <prinxard@gmail.com>
2022-11-14 16:14:55 +0100

    added creator paye menu

--- src/components/left-sidebar-1/index.js
+++ src/components/left-sidebar-1/index.js
@@ -7,7 +7,7 @@ import jwt from 'jsonwebtoken';
 
 const LeftSidebar = () => {
   const { navigation, authentication, individualNavigation,
-    navigationApprover, navigationCreator, navigationCreatorApprover, navigationAdmin, navigationReport } = useSelector(
+    navigationApprover, navigationApproverTcc, navigationCreator, navigationCreatorTcc, navigationCreatorApprover, navigationAdmin, navigationReport } = useSelector(
       (state) => ({
         navigationAdmin: state.navigationAdmin,
         navigation: state.navigation,
@@ -15,8 +15,10 @@ const LeftSidebar = () => {
         individualNavigation: state.individualNavigation,
         navigationApprover: state.navigationApprover,
         navigationCreator: state.navigationCreator,
+        navigationCreatorTcc: state.navigationCreatorTcc,
         navigationCreatorApprover: state.navigationCreatorApprover,
         navigationReport: state.navigationReport,
+        payeTccApprover: state.payeTccApprover,
       }),
       shallowEqual
     );
@@ -25,6 +27,8 @@ const LeftSidebar = () => {
   let creatorRange = [1, 4, 13, 15]
   let adminRange = [1]
   let reportRange = [39, 9, 20]
+  let payeTccInitiator = [29]
+  let payeTccApprover = [30]
 
   let StaffType;
   if (authentication) {
@@ -78,6 +82,55 @@ const LeftSidebar = () => {
       </div>
     );
   }
+
+  if (StaffType.some(r => payeTccInitiator.includes(r)) && StaffType.some(r => payeTccInitiator.includes(r)) && StaffType.some(r => payeTccInitiator.includes(r))) {
+    return (
+      <div className="left-sidebar left-sidebar-1">
+        <Logo />
+        {navigationCreatorTcc.map((menu, i) => (
+          <React.Fragment key={i}>
+            <Title>{menu.title}</Title>
+            <ul>
+              {menu.items.map((l0, a) => (
+                <li key={a} className="l0">
+                  <Item {...l0} />
+                  <ul>
+                    {l0.items.map((l1, b) => (
+                      <li key={b} className="l1">
+                        <Item {...l1} />
+                        <ul className="">
+                          {l1.items.map((l2, c) => (
+                            <li key={c} className="">
+                              <Item {...l2} />
+                              {/* <ul>
+                              {l2.items.map((l3, d) => (
+                                <li key={d} className="l3">
+                                  <Item {...l3} />
+                                  <ul>
+                                    {l3.items.map((l4, e) => (
+                                      <li key={e} className="l4">
+                                        <Item {...l4} />
+                                      </li>
+                                    ))}
+                                  </ul>
+                                </li>
+                              ))}
+                            </ul> */}
+                            </li>
+                          ))}
+                        </ul>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          </React.Fragment>
+        ))}
+      </div>
+    );
+  }
+
   if (StaffType.some(r => reportRange.includes(r)) ) {
     return (
       <div className="left-sidebar left-sidebar-1">
---
+++ src/redux/navigation/navigationApproverTcc.js
@@ -0,0 +1,370 @@
+import { RhmDashboard, ManageTaxpayer, ManageUser, DirectAssessment, Paye, Collections, Settings } from '../../components/Icons/index';
+
+// Added non individual navigation to side menu
+
+const initialState = [
+  {
+    title: 'Applications',
+    items: [
+      {
+        url: '/dashboard',
+        icon: <RhmDashboard />,
+        title: 'Dashboard',
+        items: [],
+      },
+
+      {
+        url: '/',
+        icon: <ManageTaxpayer />,
+        title: 'Manage Taxpayer',
+        items: [
+          // {
+          //   title: 'Dashboard',
+          //   url: '/',
+          //   items: [],
+          // },
+          {
+            title: 'Individual',
+            items: [
+              { title: 'Create', url: '/taxpayer', items: [] },
+              {
+                title: 'View',
+                url: '/reports-individual',
+                items: [],
+              },
+            ],
+          },
+          {
+            title: 'Non-Individual',
+            items: [
+              { title: 'Create', url: '/taxpayer/non-individual', items: [] },
+              {
+                title: 'View',
+                url: '/reports-non-individual',
+                items: [],
+              },
+            ],
+          },
+        ],
+      },
+
+      // {
+      //   url: '/',
+      //   icon: <ManageUser />,
+      //   title: 'Manage User',
+      //   items: [
+      //     {
+      //       title: 'Dashboard',
+      //       url: '/',
+      //       items: [],
+      //     },
+      //     {
+      //       title: 'User',
+      //       items: [
+      //         { title: 'Create', url: '/register', items: [] },
+      //         {
+      //           title: 'View',
+      //           url: '/view/users',
+      //           items: [],
+      //         },
+      //       ],
+      //     },
+      //   ],
+      // },
+
+      // {
+      //   url: '/',
+      //   icon: <DirectAssessment />,
+      //   title: 'Direct Assessment',
+      //   items: [
+      //     // {
+      //     //   title: 'Create',
+      //     //   url: '/direct-asses',
+      //     //   items: [],
+      //     // },
+      //     // {
+      //     //   title: 'Draft Assessments',
+      //     //   url: '/view/pendingdirect',
+      //     //   items: [],
+      //     // },
+      //     {
+      //       title: 'Submitted Assessments',
+      //       url: '/view/completeddirect',
+      //       items: [],
+      //     },
+      //     {
+      //       title: 'Verified BOJ',
+      //       url: '/view/listverifiedboj',
+      //       items: [],
+      //     },
+      //     {
+      //       title: 'Approved Assessments',
+      //       url: '/view/approvedasses',
+      //       items: [],
+      //     },
+      //     {
+      //       url: '/',
+      //       title: 'Report',
+      //       items: [
+      //         {
+      //           title: 'Assessment',
+      //           url: '/assessment-report',
+      //           items: [],
+      //         },
+      //         {
+      //           title: 'Unassessed Collections',
+      //           url: '/unassessed-report',
+      //           items: [],
+      //         },
+      //       ],
+      //     },
+      //     {
+      //       url: '/',
+      //       title: 'Tax Clearance (TCC)',
+      //       items: [
+      //         // {
+      //         //   url: '/tcc',
+      //         //   title: 'Create',
+      //         //   items: [],
+      //         // },
+      //         {
+      //           url: '/view/listtcc',
+      //           title: 'View',
+      //           items: [],
+      //         },
+      //         {
+      //           url: '/view/listprinttcc',
+      //           title: 'Print',
+      //           items: [],
+      //         },
+      //       ],
+      //     },
+      //   ],
+      // },
+
+      {
+        url: '/',
+        icon: <Paye />,
+        title: 'PAYE',
+        items: [
+          {
+            url: '/',
+            title: 'TCC',
+            items: [
+              {
+                title: 'Create',
+                url: '/tcc/paye',
+                items: [],
+              },
+              {
+                title: 'Draft',
+                url: '/view/listpayetcc/alltcc',
+                items: [],
+              },
+              {
+                title: 'Verified',
+                url: '/view/listpayetcc/alltcc/verified',
+                items: [],
+              },
+              {
+                title: 'Audit Checked',
+                url: '/view/listpayetcc/alltcc/audit',
+                items: [],
+              },
+              {
+                title: 'Pending E.C Sign',
+                url: '/view/listpayetcc/alltcc/approved',
+                items: [],
+              },
+              {
+                title: 'Print',
+                url: '/view/listpayetcc',
+                items: [],
+              },
+              // {
+              //   title: 'Unassessed Collections',
+              //   url: '/unassessed-report',
+              //   items: [],
+              // },
+            ],
+          },
+          {
+            url: '/',
+            title: 'Income Details',
+            items: [
+              {
+                title: 'Create',
+                url: '/pita/payslip',
+                items: [],
+              },
+              {
+                title: 'view',
+                url: '/view/payslip',
+                items: [],
+              },
+
+            ],
+          },
+          // {
+          //   url: '/',
+          //   title: 'Dashboard',
+          //   items: [],
+          // },
+          // {
+          //   url: '/',
+          //   title: 'Annual Returns',
+          //   items: [],
+          // },
+          // {
+          //   url: '/',
+          //   title: 'Remittance Schedules',
+          //   items: [],
+          // },
+          // {
+          //   url: '/reports',
+          //   title: 'Reports',
+          //   items: [],
+          // },
+        ],
+      },
+
+      {
+        url: '/',
+        icon: <Collections />,
+        title: 'Collections',
+        items: [
+          // {
+          //   url: '/',
+          //   title: 'Dashboard',
+          //   items: [],
+          // },
+          {
+            url: '/reports',
+            title: 'View',
+            items: [],
+          },
+          // {
+          //   url: '/',
+          //   title: 'Reconcilliations',
+          //   items: [],
+          // },
+          // {
+          //   url: '/',
+          //   title: 'Generate Receipt',
+          //   items: [
+          //     {
+          //       url: '/',
+          //       title: 'Create',
+          //       items: [],
+          //     },
+          //     {
+          //       url: '/',
+          //       title: 'View',
+          //       items: [],
+          //     },
+          //     {
+          //       url: '/',
+          //       title: 'Verify',
+          //       items: [],
+          //     },
+          //     {
+          //       url: '/',
+          //       title: 'Approve',
+          //       items: [],
+          //     },
+          //     {
+          //       url: '/',
+          //       title: 'Recon Report',
+          //       items: [],
+          //     },
+          //   ],
+          // },
+        ],
+      },
+
+      // {
+      //   url: '/',
+      //   icon: <Settings />,
+      //   title: 'Settings',
+      //   items: [
+      //     {
+      //       title: 'Revenue Items',
+      //       items: [
+      //         { title: 'Create', url: '/', items: [] },
+      //         {
+      //           title: 'View',
+      //           url: '/',
+      //           items: [],
+      //         },
+      //         {
+      //           title: 'Edit',
+      //           url: '/',
+      //           items: [],
+      //         },
+      //         {
+      //           title: 'Delete',
+      //           url: '/',
+      //           items: [],
+      //         },
+      //       ],
+      //     },
+      //     {
+      //       title: 'Tax Office',
+      //       items: [
+      //         { title: 'Create', url: '/', items: [] },
+      //         {
+      //           title: 'View',
+      //           url: '/',
+      //           items: [],
+      //         },
+      //         { title: 'Edit', url: '/', items: [] },
+      //         { title: 'Delete', url: '/', items: [] },
+
+      //       ],
+      //     },
+      //     {
+      //       title: 'Sectors',
+      //       items: [
+      //         { title: 'Create', url: '/', items: [] },
+      //         { title: 'View', url: '/', items: [] },
+      //         {
+      //           title: 'Edit',
+      //           url: '/',
+      //           items: [],
+      //         },
+      //         {
+      //           title: 'Delete',
+      //           url: '/',
+      //           items: [],
+      //         },
+      //       ],
+      //     },
+      //     {
+      //       title: 'Budget Estimate',
+      //       items: [
+      //         { title: 'Create', url: '/', items: [] },
+      //         { title: 'View', url: '/', items: [] },
+      //         {
+      //           title: 'Edit',
+      //           url: '/',
+      //           items: [],
+      //         },
+      //         {
+      //           title: 'Delete',
+      //           url: '/',
+      //           items: [],
+      //         },
+      //       ],
+      //     },
+      //   ],
+      // },
+    ],
+  },
+];
+
+export default function navigationApprover(state = initialState, action) {
+  switch (action.type) {
+    default:
+      return state;
+  }
+}
