ae94ec355fc4d0a327d6aaa261ea9e5456145159
prince unachukwu <prinxard@gmail.com>
2023-01-11 13:39:37 +0100

    added annual filing count

--- src/pages/paye-annual/index.js
+++ src/pages/paye-annual/index.js
@@ -16,57 +16,60 @@ function index() {
     const [FilteredData, setFilteredData] = useState([]);
     const [isFetching, setIsFetching] = useState(false);
     const [tableState, setTableState] = useState("hidden");
-    const router = useRouter()
-    const {
-        register,
-        handleSubmit,
-        control,
-        watch,
-        formState: { dirtyFields }
-    } = useForm(
-        { mode: "onBlur", }
-    )
+    const [yearReport, setYearReport] = useState([]);
+
+
     const selectedYear = dateformat(startDate, "yyyy")
+
     const SearchYear = () => {
         setAuthToken()
         console.log("selectedYear", selectedYear);
         setIsFetching(true)
         axios.get(`${url.BASE_URL}annual/view-annual-year?year=${selectedYear}`)
             .then(function (response) {
+                axios.post(`${url.BASE_URL}annual/report`, { "year": selectedYear })
+                    .then(function (responsedata) {
+                        let report = responsedata.data.body.report[0]
+                        setYearReport(report)
+                        console.log("responsedata", report);
+                    })
                 let search = response.data.body;
-                console.log("search", search);
                 setFilteredData(search)
                 console.log("FilteredData", FilteredData);
                 setIsFetching(false)
                 setTableState('')
-            })
-            .catch(function (error) {
-                setTableState('')
-                setIsFetching(false)
 
             })
     }
-
+    console.log("yearReport", yearReport);
     return (
         <>
-            <div className="overflow-x-auto my-4 max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-4">
-                <p className="font-bold mb-5">Select Year</p>
-                <div className="">
-                    <ReactDatePicker
-                        selected={startDate}
-                        onChange={(date) => setStartDate(date)}
-                        showYearPicker
-                        dateFormat="yyyy"
-                        className="form-control mb-3 rounded"
-                    />
+            <div className="overflow-x-auto flex justify-around my-4 max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-4">
+                <div>
+                    <p className="font-bold mb-5">Select Year</p>
+                    <div className="">
+                        <ReactDatePicker
+                            selected={startDate}
+                            onChange={(date) => setStartDate(date)}
+                            showYearPicker
+                            dateFormat="yyyy"
+                            className="form-control mb-3 rounded"
+                        />
+                    </div>
+                    <div>
+                        <button className="btn w-32 bg-green-600 btn-default text-white btn-outlined bg-transparent rounded-md"
+                            type="submit"
+                            onClick={() => SearchYear()}
+                        >
+                            Search
+                        </button>
+                    </div>
                 </div>
                 <div>
-                    <button className="btn w-32 bg-green-600 btn-default text-white btn-outlined bg-transparent rounded-md"
-                        type="submit"
-                        onClick={() => SearchYear()}
-                    >
-                        Search
-                    </button>
+                    <p className="flex justify-between"> <span className="mr-3">No. of Submitted</span> <span className="font-bold">{yearReport?.Submitted || 0}</span></p>
+                    <p className="flex justify-between"> <span className="mr-3">No. of Verified</span> <span className="font-bold">{yearReport?.Verified || 0}</span></p>
+                    <p className="flex justify-between"> <span className="mr-3">No. of Approved</span> <span className="font-bold">{yearReport?.Approved || 0}</span></p>
+                    <p className="flex justify-between"> <span className="mr-3">No. of Declined</span> <span className="font-bold">{yearReport?.Declined || 0}</span></p>
                 </div>
             </div>
             {isFetching ? (
