a96f5b837ad93f7c53947af257bdd4cec2f35c0f
prince unachukwu <prinxard@gmail.com>
2023-04-05 14:37:37 +0100

    pushed objection to live

--- src/config/url.js
+++ src/config/url.js
@@ -4,12 +4,12 @@ const url = {
   
   // PAY_URL: 'http://localhost/etaxpay/',
 
-  
+
   // live db
 
-  BASE_URL: "https://rhmapi.bespoque.dev/api/v1/",
+  // BASE_URL: "https://rhmapi.bespoque.dev/api/v1/",
 
-  // BASE_URL: "https://rhmlive.bespoque.dev/api/v1/",
+  BASE_URL: "https://rhmlive.bespoque.dev/api/v1/",
 
   // PAY_URL: "https://etaxpay.irs.kg.gov.ng/",
 
