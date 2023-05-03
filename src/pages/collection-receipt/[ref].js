e6974b98340bb77838395efb951f26535558afa8
prinxard <prinxard@gmail.com>
2023-05-03 22:58:44 +0100

    added print receipt for assessment ID

--- src/pages/collection-receipt/[ref].js
+++ src/pages/collection-receipt/[ref].js
@@ -15,135 +15,192 @@ export default function index() {
     const [colData, setColData] = useState([]);
     const router = useRouter();
     const componentRef = useRef();
+    const [isFetching, setIsFetching] = useState(true);
+    const urlNew = "https://bespoque.dev/quickpay-live/"
     useEffect(() => {
+        setAuthToken()
         if (router && router.query) {
-            let paymentID = router.query.ref;
-            // if paymentID.includes("FA")
+            let paymentID = String(router.query.ref);
             let paymentPayload = {
                 "idpymt": paymentID
             }
-            setAuthToken();
             const fetchPost = async () => {
-                try {
-                    let res = await axios.post(`${url.BASE_URL}collection/view-collections`, paymentPayload);
-                    res = res.data.body;
-                    setColData(res)
-                } catch (e) {
-                    console.log(e);
+                setIsFetching(true)
+                if (paymentID.includes("FA")) {
+                    try {
+                        let response = await fetch(`${urlNew}getpayment.php?paymentref=${paymentID}&by=assessment`, {
+                            method: 'GET',
+                        });
+                        
+                        const data = await response.json()
+                        setIsFetching(false)
+                        let res = [data.body];
+                        setColData(res)
+                        console.log("res", res);
+                    } catch (e) {
+                        console.log(e);
+                        setIsFetching(false)
+                    }
+                    // axios.get(`${urlNew}getpayment.php?paymentref=${paymentID}&by=assessment`)
+                    //     .then(function (response) {
+                    //         let res = response.data.body;
+                    //         setColData(() => [response.data.body])
+                    //         console.log("res", res);
+                    //         setIsFetching(false)
+                    //     })
+                    //     .catch(function (error) {
+                    //         setIsFetching(false)
+                    //         console.log(error);
+                    //     })
+                }
+                else {
+                    try {
+                        let res = await axios.post(`${url.BASE_URL}collection/view-collections`, paymentPayload);
+                        res = res.data.body;
+                        setColData(res)
+                    } catch (e) {
+                        console.log(e);
+                        setIsFetching(false)
+                    }
                 }
             };
+            // const fetchPost = async () => {
+            //     try {
+            //         let res = await axios.post(`${url.BASE_URL}collection/view-collections`, paymentPayload);
+            //         res = res.data.body;
+            //         setColData(res)
+            //     } catch (e) {
+            //         console.log(e);
+            //     }
+            // };
             fetchPost();
         }
     }, [router]);
 
     return (
-        <div className='rounded-lg p-6 bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-800'>
-            <div className="m-3 flex justify-end">
-                <div>
-                    <ReactToPrint
-                        pageStyle='@page { size: auto; margin: 0mm; } @media print { body { -webkit-print-color-adjust: exact; padding: 40px !important; } }'
-                        // pageStyle="@page { size: 7.5in 13in  }"
-                        trigger={() => <button className="btn w-32 bg-green-600 btn-default text-white
-                            btn-outlined bg-transparent rounded-md"
-                            type="submit"
-                        >
-                            Print
-                        </button>}
-                        content={() => componentRef.current}
+        <>
+            {isFetching && (
+                <div className="flex justify-center item mb-2">
+                    <Loader
+                        visible={isFetching}
+                        type="BallTriangle"
+                        color="#00FA9A"
+                        height={19}
+                        width={19}
+                        timeout={0}
+                        className="ml-2"
                     />
+                    <p>Fetching data...</p>
                 </div>
+            )}
+            <div className='rounded-lg p-6 bg-white border border-gray-100 dark:bg-gray-900 dark:border-gray-800'>
+                <div className="m-3 flex justify-end">
+                    <div>
+                        <ReactToPrint
+                            pageStyle='@page { size: auto; margin: 0mm; } @media print { body { -webkit-print-color-adjust: exact; padding: 40px !important; } }'
+                            // pageStyle="@page { size: 7.5in 13in  }"
+                            trigger={() => <button className="btn w-32 bg-green-600 btn-default text-white
+                            btn-outlined bg-transparent rounded-md"
+                                type="submit"
+                            >
+                                Print
+                            </button>}
+                            content={() => componentRef.current}
+                        />
+                    </div>
 
+                </div>
+                {colData.map((el, i) => (
+                    <div className="border p-6" ref={componentRef}>
+                        <p>KOGI STATE GOVERNMENT</p>
+                        <section className="flex justify-between">
+                            <p className="font-bold">REVENUE RECEIPT</p>
+                            <p className="font-bold">{el.ref}</p>
+                        </section>
+                        <section className="flex justify-end mt-8">
+                            <CoatOfArms />
+                            <KogiGov />
+                            <KgirsLogo />
+                        </section>
+                        <div className="flex justify-between">
+                            <div>
+                                <div className="grid grid-cols-6 gap-2">
+                                    <p>PAID BY:</p>
+                                    <p className="font-bold col-span-2">{el.taxpayerName}</p>
+                                </div>
+                                <div className="grid grid-cols-6 gap-2">
+                                    <p>PAYER ID:</p>
+                                    <p className="font-bold col-span-2">{el.t_payer}</p>
+                                </div>
+                                <div className="grid grid-cols-6 gap-2">
+                                    <p>ADDRESS:</p>
+                                    <p className="font-bold col-span-2">{el.taxpayerAddress}</p>
+                                </div>
+                                <div className="flex mt-10">
+                                    <div className='w-16 border-b-2'>
+                                    </div>
+                                    <p className='align-self-center'>Details</p>
+                                    <div className="border-b-2 w-3/4 ">
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="mt-6 mr-6">
+                                <QRCode
+                                    value={`https://irs.kg.gov.ng/verify/verify_receipt.php?ref=${el.ref}`}
+                                    size={120}
+                                />
+                            </div>
+
+                        </div>
+                        <div className="mt-3">
+                            <div className="grid grid-cols-6 gap-2">
+                                <p>PAYMENT DATE:</p>
+                                <p className="font-bold col-span-2">{el.tran_date}</p>
+                            </div>
+                            <div className="grid grid-cols-6 gap-2">
+                                <p>AMOUNT:</p>
+                                <div className="col-span-4">
+                                    <p className="font-bold">NGN {formatNumber(el.amount)}</p>
+                                    {/* <small>Eighty thousand seven hundred and thirty two naira only</small> */}
+                                </div>
+                            </div>
+                            <div className="grid grid-cols-6 gap-2">
+                                <p>BEING:</p>
+                                <div className="col-span-3">
+                                    <p className="font-bold"> {`Payment for (${el.rev_sub})`} </p>
+                                    <small>{el.revenueItem}</small>
+                                </div>
+                            </div>
+                            <div className="grid grid-cols-6 gap-2">
+                                <p>PAID AT:</p>
+                                <p className="font-bold"> {el.bank} </p>
+                            </div>
+                            <div className="grid grid-cols-6 gap-2">
+                                <p>AGENCY:</p>
+                                <div className="col-span-3">
+                                    <p className="font-bold"> INTERNAL REVENUE SERVICE </p>
+                                </div>
+                            </div>
+                            <div className="grid grid-cols-6 gap-2">
+                                <p>TAX STATION:</p>
+                                <p className="font-bold"> {el.station} </p>
+                            </div>
+                            <div className="border-b-2 mt-3 w-4/4 ">
+                            </div>
+                        </div>
+
+                        <div className="flex justify-between">
+                            <div></div>
+                            <div className="mt-2">
+                                <SignatureCol />
+                                <hr />
+                                Authorized Signatory
+                            </div>
+                        </div>
+                    </div>
+                ))}
             </div>
-            {colData.map((el, i) => (
-                <div className="border p-6" ref={componentRef}>
-                    <p>KOGI STATE GOVERNMENT</p>
-                    <section className="flex justify-between">
-                        <p className="font-bold">REVENUE RECEIPT</p>
-                        <p className="font-bold">{el.ref}</p>
-                    </section>
-                    <section className="flex justify-end mt-8">
-                        <CoatOfArms />
-                        <KogiGov />
-                        <KgirsLogo />
-                    </section>
-                    <div className="flex justify-between">
-                        <div>
-                            <div className="grid grid-cols-6 gap-2">
-                                <p>PAID BY:</p>
-                                <p className="font-bold col-span-2">{el.taxpayerName}</p>
-                            </div>
-                            <div className="grid grid-cols-6 gap-2">
-                                <p>PAYER ID:</p>
-                                <p className="font-bold col-span-2">{el.t_payer}</p>
-                            </div>
-                            <div className="grid grid-cols-6 gap-2">
-                                <p>ADDRESS:</p>
-                                <p className="font-bold col-span-2">{el.taxpayerAddress}</p>
-                            </div>
-                            <div className="flex mt-10">
-                                <div className='w-16 border-b-2'>
-                                </div>
-                                <p className='align-self-center'>Details</p>
-                                <div className="border-b-2 w-3/4 ">
-                                </div>
-                            </div>
-                        </div>
-                        <div className="mt-6 mr-6">
-                            <QRCode
-                                value={`https://irs.kg.gov.ng/verify/verify_receipt.php?ref=${el.ref}`}
-                                size={120}
-                            />
-                        </div>
-
-                    </div>
-                    <div className="mt-3">
-                        <div className="grid grid-cols-6 gap-2">
-                            <p>PAYMENT DATE:</p>
-                            <p className="font-bold col-span-2">{el.tran_date}</p>
-                        </div>
-                        <div className="grid grid-cols-6 gap-2">
-                            <p>AMOUNT:</p>
-                            <div className="col-span-4">
-                                <p className="font-bold">NGN {formatNumber(el.amount)}</p>
-                                {/* <small>Eighty thousand seven hundred and thirty two naira only</small> */}
-                            </div>
-                        </div>
-                        <div className="grid grid-cols-6 gap-2">
-                            <p>BEING:</p>
-                            <div className="col-span-3">
-                                <p className="font-bold"> {`Payment for (${el.rev_sub})`} </p>
-                                <small>{el.revenueItem}</small>
-                            </div>
-                        </div>
-                        <div className="grid grid-cols-6 gap-2">
-                            <p>PAID AT:</p>
-                            <p className="font-bold"> {el.bank} </p>
-                        </div>
-                        <div className="grid grid-cols-6 gap-2">
-                            <p>AGENCY:</p>
-                            <div className="col-span-3">
-                                <p className="font-bold"> INTERNAL REVENUE SERVICE </p>
-                            </div>
-                        </div>
-                        <div className="grid grid-cols-6 gap-2">
-                            <p>TAX STATION:</p>
-                            <p className="font-bold"> {el.station} </p>
-                        </div>
-                        <div className="border-b-2 mt-3 w-4/4 ">
-                        </div>
-                    </div>
-
-                    <div className="flex justify-between">
-                        <div></div>
-                        <div className="mt-2">
-                            <SignatureCol />
-                            <hr />
-                            Authorized Signatory
-                        </div>
-                    </div>
-                </div>
-            ))}
-        </div>
+        </>
     )
 }
 
